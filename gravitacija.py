ed78e9a321ef5a3e993b3687e27db4d6051a9af1
mjaksetic <manca.jaksetic@gmail.com>
2024-02-29 17:51:43 +0100

    Dodaj izračun pospeška #3

--- gravitacija.py
+++ gravitacija.py
@@ -1,1 +1,11 @@
 print("OIS je zakon!")
+
+nadmvisina = input()
+
+C = 6.674 * 10**-11
+M = 5.972 * 10**24
+r = 6.371 * 10**6
+
+def izracun(nadmvisina):
+    a = (C * M) / (r + nadmvisina)**2
+    return a
